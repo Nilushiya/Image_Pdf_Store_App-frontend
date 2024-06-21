e63d2628d7b9bd6dc6b22bbad6f1166eee7a046c
Nilushiya <nilushiyak@gmail.com>
2024-06-21 12:49:24 +0530

    profile icons added

--- src/Components/Action.jsx
+++ src/Components/Action.jsx
@@ -3,6 +3,8 @@ import action from './Style/Action.css'
 import { Link} from 'react-router-dom';
 import { decodeToken } from '../Contaxt/UserContaxt';
 import Navbar from './Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faCamera, faEye } from '@fortawesome/free-solid-svg-icons'; 
 
 const Action = () => {
   const [user_name , setUser_name] = useState('');
@@ -11,14 +13,26 @@ const Action = () => {
   useEffect(() => {
     const decode = decodeToken();
      const user_name = decode.user_name;
-     const firstLetter = user_name.charAt(0).toUpperCase();
-     console.log("first : ", firstLetter)
      setUser_name(user_name)
+     const firstLetter = user_name.charAt(0).toUpperCase();
+     setName_firstLetter(firstLetter)
+     console.log("first : ", firstLetter)
   },[])
-  
+
     return (
         <div className="container">
           <Navbar />
+          <div className="profile-circle">
+           {name_firstLetter ? <p style={{fontSize:"40px"}}>{name_firstLetter}</p> : <FontAwesomeIcon icon={faCamera} className="fa-2x" />}
+          </div>
+          <div className="profile-actions">
+            <Link className="icon-button" to='/profile/edit'>
+              <FontAwesomeIcon icon={faEye}  />
+            </Link>
+            <Link className="icon-button" to='/profile/view'>
+              <FontAwesomeIcon icon={faEdit}  />
+            </Link>
+          </div>
           <h1 className="heading">Choose an Action</h1>
           <div className="button-container">
             <Link className="animated-button" to='./image'>Image</Link>
