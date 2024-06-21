b0a7cc224aab59e08afe97b66b1932b73b7591c5
Nilushiya <nilushiyak@gmail.com>
2024-06-21 16:18:33 +0530

    profile popup created

--- src/Components/Action.jsx
+++ src/Components/Action.jsx
@@ -1,45 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import action from './Style/Action.css'
-import { Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { decodeToken } from '../Contaxt/UserContaxt';
 import Navbar from './Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faCamera, faEye } from '@fortawesome/free-solid-svg-icons'; 
+import { faEdit, faEye, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Modal, Button } from 'react-bootstrap';
+import './Style/Action.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Action = () => {
-  const [user_name , setUser_name] = useState('');
-  const [name_firstLetter , setName_firstLetter] = useState('');
+  const [user_name, setUser_name] = useState('');
+  const [name_firstLetter, setName_firstLetter] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const decode = decodeToken();
-     const user_name = decode.user_name;
-     setUser_name(user_name)
-     const firstLetter = user_name.charAt(0).toUpperCase();
-     setName_firstLetter(firstLetter)
-     console.log("first : ", firstLetter)
-  },[])
+    const user_name = decode.user_name;
+    setUser_name(user_name);
+    const firstLetter = user_name.charAt(0).toUpperCase();
+    setName_firstLetter(firstLetter);
+    console.log("first : ", firstLetter);
+  }, []);
 
-    return (
-        <div className="container">
-          <Navbar />
-          <div className="profile-circle">
-           {name_firstLetter ? <p style={{fontSize:"40px"}}>{name_firstLetter}</p> : <FontAwesomeIcon icon={faCamera} className="fa-2x" />}
-          </div>
-          <div className="profile-actions">
-            <Link className="icon-button" to='/profile/edit'>
-              <FontAwesomeIcon icon={faEye}  />
-            </Link>
-            <Link className="icon-button" to='/profile/view'>
-              <FontAwesomeIcon icon={faEdit}  />
-            </Link>
-          </div>
-          <h1 className="heading">Choose an Action</h1>
-          <div className="button-container">
-            <Link className="animated-button" to='./image'>Image</Link>
-            <Link className="animated-button" to='./pdf'>PDF</Link>
-          </div>
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+
+  return (
+    <>
+      <div className="container">
+        <Navbar />
+        <div className="profile-circle">
+          <img className='image' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpHAFH61T04_MydAWCka1ncFaA3ASrYY6v-g&s' alt="Profile" />
         </div>
-      );
+        <div className="profile-actions">
+          <button className="icon-button" onClick={handleShow}>
+            <FontAwesomeIcon icon={faEye} />
+          </button>
+          <Link className="icon-button" to='/profile/view'>
+            <FontAwesomeIcon icon={faEdit} />
+          </Link>
+        </div>
+        <h1 className="heading">Choose an Action</h1>
+        <div className="button-container">
+          <Link className="animated-button" to='./image'>Image</Link>
+          <Link className="animated-button" to='./pdf'>PDF</Link>
+        </div>
+      </div>
+
+      <Modal show={showModal} onHide={handleClose} centered className='model '> 
+     
+       <div className='modelbox'>
+          <h1 className='head' style={{color:"white"}}> <FontAwesomeIcon icon={faUser} /></h1>
+          <p>Username: {user_name}</p>
+          <p>Username: {user_name}</p>
+          <p>Username: {user_name}</p>
+          <Button variant="secondary" onClick={handleClose} className='modelFooter '>
+            Close
+          </Button>
+       </div>
+      </Modal>
+    </>
+  );
 }
 
-export default Action
+export default Action;
---
+++ src/Components/UserDetailsPopup.jsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import { Modal, Button } from 'react-bootstrap';
+// import userDetails from './Style/UserDetails.css'
+// const UserDetailsPopup = ({ show, handleClose, user }) => {
+//   return (
+//     <Modal show={show} onHide={handleClose}>
+//       <Modal.Header closeButton>
+//         <Modal.Title>User Details</Modal.Title>
+//       </Modal.Header>
+//       <Modal.Body>
+//         <p><strong>Username:</strong> {user.user_name}</p>
+//         {/* Add more user details here */}
+//       </Modal.Body>
+//       <Modal.Footer>
+//         <Button variant="secondary" onClick={handleClose}>
+//           Close
+//         </Button>
+//       </Modal.Footer>
+//     </Modal>
+//   );
+// };
+
+// export default UserDetailsPopup;
